565cf2b58c97ba24a4a4e037fa41798914ad76eb
vitaliy <vitaliy@backbase.com>
2021-10-11 23:26:10 +0300

    Conveyor initial commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Conveyor
+
 JS: Live conveyor line with arriving tasks and its handler (with visualisation)
+
+###Task:
+
+There is a stream of incoming tasks (conveyor line), each can take a different time to be done.
+We should create a conveyor to operate tasks from queue with limitation put on in-progress quantity.
+
+###Approach:
+
+1) Create a queue with incoming tasks within some interval with a special prop, determining task operating time.
+2) There should be a task going through the list and mark a task `in progress`, but not more then `max` quantity (provided as a param).
+3) When the `operating time`'s out - mark finished task as `done` and start a new one from queue, but not more then a certain threshold.
+4) Visualise the process
+
+![](./conveyor.png)
+- gray -- idle (waiting) tasks
+- green -- tasks in progress
+- black -- done tasks
+- numbers inside blocks -- piece of work to perform the task (i.e. amount of time to finish task)
---
+++ limited-stream.mjs
@@ -0,0 +1,89 @@
+export class Conveyor {
+  constructor(el, tasksLimit = 3) {
+    this.tasksInProgressTheshold = tasksLimit;
+    this.shouldStopTicking = false;
+    this.taskInterval = 1000;
+    this.inProgressCounter = 0;
+    this.el = el;
+    this.line = [];
+  }
+
+  startConveyor(inputLimit = 10) {
+    this.initInputTasksQueue(inputLimit);
+  }
+
+  initInputTasksQueue(inputLimit) {
+    let limitCounter = 0;
+    const counter = setInterval(() => {
+      this.addTaskToQueue(limitCounter);
+      limitCounter++;
+      if (limitCounter >= inputLimit) {
+        clearInterval(counter);
+        this.startTrackerTicking();
+      }
+      this.visualize();
+    }, this.taskInterval/5)
+  }
+
+  startTrackerTicking(interval = 100) {
+    const tick = setInterval(() => {
+      if (this.shouldStopTicking) {
+        clearInterval(tick);
+      }
+      this.doCheck();
+      this.visualize();
+    }, interval);
+  }
+
+  doCheck() {
+    let allDone = true;
+    if (this.line.length) {
+      this.line.forEach((task) => {
+        if (!task.finished && !task.inWork && this.inProgressCounter < this.tasksInProgressTheshold) {
+          this.inProgressCounter++;
+          task.inWork = true;
+          task.startWork();
+        }
+        allDone = allDone && task.finished;
+      });
+
+      this.shouldStopTicking = allDone;
+    }
+  }
+
+  addTaskToQueue(id) {
+    const task = {
+      id,
+      workToDo: Math.floor(Math.random() * 10) + 1,
+      inWork: false,
+      finished: false,
+      startWork: () => {
+        if (!task.finished) {
+          const starter = setInterval(() => {
+            if (task.workToDo > 0) {
+              task.workToDo--
+            } else {
+              task.inWork = false;
+              task.finished = true;
+              this.inProgressCounter--;
+              clearInterval(starter);
+            }
+          }, this.taskInterval)
+        }
+      },
+    }
+
+    this.line.push(task);
+  }
+
+  visualize() {
+    if (this.el) {
+      const items = this.line.map(item => {
+        const progressClass = item.inWork ? ' inWork' : item.finished ? ' finished' : ' idle';
+        return `<div class="item${progressClass}">${item.workToDo}</div>`
+      }).join('');
+
+      this.el.innerHTML = `<div>${items}</div>`;
+    }
+  }
+}
